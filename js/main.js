e99d4dabed4bf6a129c7c4ab647caecd15016d26
Davlet Ganiev <Ganiev.Davlet@gmail.com>
2021-07-11 18:21:21 +0300

    create landing

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $('.section-three-carousel').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.section-seven-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
