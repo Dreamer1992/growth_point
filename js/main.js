9c7cae3b9fd729a3120e33e06862354ab284cc8c
Davlet Ganiev <Ganiev.Davlet@gmail.com>
2021-07-19 23:26:50 +0300

    create landing

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $('.section-three-carousel').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><img src='./img/common/arrow_purple.webp'></button>",
+        nextArrow: "<button type='button' class='slick-next pull-left'><img src='./img/common/arrow_purple.webp'></button>",
         responsive: [
             {
                 breakpoint: 600,
@@ -22,6 +24,8 @@ $(document).ready(function () {
     $('.section-seven-carousel').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><img src='./img/common/arrow_purple.webp'></button>",
+        nextArrow: "<button type='button' class='slick-next pull-left'><img src='./img/common/arrow_purple.webp'></button>",
         responsive: [
             {
                 breakpoint: 600,
@@ -39,25 +43,49 @@ $(document).ready(function () {
         ]
     });
 
-    $('.section-ten_carousel-left, .section-ten_carousel-right').slick({
+    // $('.section-ten_carousel-left, .section-ten_carousel-right').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    // });
+
+    $('.section-ten_carousel-left').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><img src='./img/common/arrow_orange.webp'></button>",
+        nextArrow: "<button type='button' class='slick-next pull-left'><img src='./img/common/arrow_orange.webp'></button>",
+    });
+
+    $('.section-ten_carousel-right').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><img src='./img/common/arrow_blue.webp'></button>",
+        nextArrow: "<button type='button' class='slick-next pull-left'><img src='./img/common/arrow_blue.webp'></button>",
     });
 
     $('.reviews-carousel_nav').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
         centerMode: true,
+        centerPadding: '60px',
+        prevArrow: "<button type='button' class='slick-prev pull-left'><img src='./img/common/arrow_purple.webp'></button>",
+        nextArrow: "<button type='button' class='slick-next pull-left'><img src='./img/common/arrow_purple.webp'></button>",
         asNavFor: '.reviews-carousel_for',
         focusOnSelect: true,
         responsive: [
             {
-                breakpoint: 600,
+                breakpoint: 768,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
+                    arrows: false,
+                    slidesToShow: 2
                 }
             },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1
+                }
+            }
         ]
     });
 
